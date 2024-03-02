f3246a804358fa550fd5c07ef3ac061ae45c6575
Krekotend <109600012+Krekotend@users.noreply.github.com>
2024-03-02 12:39:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 import asyncio
 import sql_service
 import service
+import logging
 from decouple import config
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import UserBlocked, UserDeactivated
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 ID = config('ID')
 HASH = config('HASH')
 
 app = Client("me", ID, HASH)
 
+async def send_message1(id):
+    await app.send_message(id, "Это сообщение 1")
 
-@app.on_message(filters.text & filters.private)
-async def process_message(client, message: Message):
-    await app.send_message(chat_id=message.from_user.id,text = 'tep')
-    async with await sql_service.create_session() as session:
-        await sql_service.chek_client_id(session, client_id=message.chat.id)
-        if message.chat.id == client.me.id:
-            if "прекрасно" in message.text.lower() or "ожидать" in message.text.lower():
-                await sql_service.cancel(client, message)
-            else:
-                await sql_service.cancel_msg2(session, message.chat.id)
-        await session.commit()
+async def send_message2(id):
+    await app.send_message(id, "Это сообщение 2")
 
+async def send_message3(id):
+    await app.send_message(id, "Это сообщение 3")
 
 @app.on_message()
-async def send_message1(client, message: Message):
-    await client.send_message(message.chat.id, "Это сообщение 1")
-
-@app.on_message(filters.command("send_message2"))
-async def send_message2(client, message: Message):
+async def process_message(client, message: Message):
     async with await sql_service.create_session() as session:
-        await sql_service.create_client_and_msg2(session, message.chat.id)
-        await client.send_message(message.chat.id, "Это сообщение 2")
+        await sql_service.check_client_id(session, client_id=message.from_user.id)
+    if message.from_user.id == client.me.id:
+        if "прекрасно" in message.text.lower() or "ожидать" in message.text.lower():
+            await sql_service.cancel(message.from_user.id, message)
+        else:
+            await sql_service.cancel_msg2(session, message.chat.id)
         await session.commit()
-
-
-@app.on_message(filters.command("send_message3"))
-async def send_message3(client, message: Message):
-    async with await sql_service.create_session() as session:
-        await sql_service.create_client_and_msg3(session, message.chat.id)
-        await client.send_message(message.chat.id, "Это сообщение 3")
-        await session.commit()
-
 
 @app.on_message()
 async def error_handler(client, error):
@@ -56,10 +46,16 @@ async def error_handler(client, error):
 
 
 async def main():
-    await app.start()
-    await asyncio.Future()
-    await service.run()
-
+    while True:
+        await asyncio.gather(
+            service.run(),
+            app.start()
+            )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run(main())
+
+#     Выдает ошибку
+#     raise ConnectionError("Client has not been started yet")
+# ConnectionError: Client has not been started yet
+
